191b8b3d4e328108dc186c113d2a08215d994344
Alapottra <alapottratrojon@gmail.com>
2022-02-13 19:27:05 +0600

    added pin verification functionality

--- script.js
+++ script.js
@@ -13,31 +13,41 @@ function generateRandomPin() {
 
 generatePinBtn.addEventListener("click", function () {
   const randomPin = generateRandomPin();
-  const randomNumberDisplayed = document.querySelector("#random-number-display");
+  const randomNumberDisplayed = document.querySelector(
+    "#random-number-display"
+  );
   randomNumberDisplayed.value = randomPin;
 });
 
-
-
 const calcBody = document.querySelector(".numbers-tab");
-let inputDisplay = document.querySelector('.input-display');
+let inputDisplay = document.querySelector(".input-display");
 
 calcBody.addEventListener("click", function (e) {
-    let previousNum = document.querySelector('.input-display').value;
-    let num = e.target.innerText;
+  let previousNum = document.querySelector(".input-display").value;
+  let num = e.target.innerText;
 
-    if(num.length == 1){
-       if(num == "C"){
-        inputDisplay.value = "";
-        
-       }
-       else{
-        inputDisplay.value = previousNum + num ;
-       }
-        
-       
+  if (num.length == 1) {
+    if (num == "C") {
+      inputDisplay.value = "";
+    } else {
+      inputDisplay.value = previousNum + num;
+    }
+  }
+});
 
 
-    } 
+const sumbitBtn = document.querySelector(".submit-btn");
 
-})
+sumbitBtn.addEventListener("click", function (){
+    let generatedPin = document.querySelector("#random-number-display").value;
+    let inputPin = document.querySelector(".input-display").value;
+    // let parsedGeneratedPin =parseInt(generatedPin);
+    // let parsedInputPin =parseInt(InputPin);
+
+    if(generatedPin == inputPin){
+        console.log("macthed");
+    }else{
+        console.log('ops');
+    }
+
+});
